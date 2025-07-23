169af626c289f854450f937e6cab4ba7a8965b6e
abdelrahman <hgf20kljv@gmail.com>
2025-07-23 10:41:34 +0300

    1.1

--- old.py
+++ old.py
@@ -1,39 +1,44 @@
-lstname=[]
+import requests
+
+FIREBASE_URL = "https://any-thing-93729-default-rtdb.firebaseio.com/students.json"
+
 for i in range(1,3):
-     print('the students' ,i )
-     x=input("Enter the list member : ",)  
-     lstname.append(x)
+     print('the students', i)
+     student = {}
+     x = input("Enter the student name: ")  
+     student['name'] = x
+     grades = {}
      for j in range(1,5):
-          if j==1 :
-               s='Arbic'
-          elif j==2 :
-               s='math'
-          elif j==3 :
-               s='english'
-          else :
-               s='science'    
-          print(s)
-          m=(input('Enter the students dagree :',))
-          if m.isdigit():
-               m=float(m)
-               if m >= 90 : 
-                    y=("excllent")
-               elif m >= 80 :
-                    y=("very good ")
-               elif m >= 60 :        
-                    y=("good")   
-               elif m >= 50 :
-                    y=("pass")
-               else: 
-                   y=('fail')
-
-               lstname.append(s)
-               lstname.append(m)
-               lstname.append(y)
-
+          if j == 1:
+               s = 'Arbic'
+          elif j == 2:
+               s = 'math'
+          elif j == 3:
+               s = 'english'
           else:
-               print("wrong entery _tryAgain")
-               j-=1  
-
-print(lstname)
+               s = 'science'    
+          print(s)
+          m = input('Enter the student degree: ')
+          if m.isdigit():
+               m = float(m)
+               if m >= 90:
+                    y = "excellent"
+               elif m >= 80:
+                    y = "very good"
+               elif m >= 60:
+                    y = "good"
+               elif m >= 50:
+                    y = "pass"
+               else:
+                    y = 'fail'
+               grades[s] = {'degree': m, 'result': y}
+          else:
+               print("wrong entry _tryAgain")
+               j -= 1 
+     student['grades'] = grades
+     response = requests.post(FIREBASE_URL, json=student)
+     if response.status_code == 200:
+          print("Student data has been sent successfully !")
+     else:
+          print("An error occurred while sending the data:", response.text)
  
